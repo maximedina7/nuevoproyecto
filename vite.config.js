61d16a37dcf66784a21be0c2d00be16f1bb1e6c7
maximedina7 <maxidomedina@gmail.com>
2025-04-29 18:57:38 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/nuevoproyecto2/',
-  plugins: [react()],
-})
+  base: '/nuevoproyecto2/', // o '/' si estás en la raíz del dominio
+  build: {
+    outDir: 'docs',
+  },
+});
+
